18e190802522732a21d152a4dd5b6994d9bfb77b
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-08-28 11:43:04 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, onMounted } from "vue";
 import axios from "axios";
 import App from "./App.vue";
 import { createPinia } from "pinia";
@@ -15,8 +15,19 @@ import "ant-design-vue/dist/reset.css";
 import apiURL from "./api/constants.js";
 import Config from './api/config/config.js';
 // import CKEditor from "@ckeditor/ckeditor5-vue";
-
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 window.axios = axios;
+window.Pusher = Pusher;
+window.Echo = new Echo({
+	broadcaster: 'pusher',
+	key: '72b5ec4ddd643cd15ea0',
+	cluster: 'mt1',
+	forceTLS: true,
+	broadcasterOptions: {
+        authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth'
+    }
+});
 
 router.beforeEach((to, from, next) => {
 	window.scrollTo(0, 0);
@@ -35,33 +46,33 @@ app.mount("#app");
 
 async function updateTitle() {
 	try {
-	  const { getconfig, responseConfig } = Config();
-	  await getconfig(); 
-	  const configData = responseConfig.data; 
-	  
-	  if (configData && configData.title) {
-		document.title = configData.title;
-	  } else {
-		document.title = 'Bật động sản'; 
-	  }
+		const { getconfig, responseConfig } = Config();
+		await getconfig();
+		const configData = responseConfig.data;
 
-	  if (configData && configData.icon) {
-		const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
-		favicon.type = 'image/svg+xml';
-		favicon.rel = 'icon';
-		favicon.href = configData.icon;
-		document.head.appendChild(favicon);
-	  } else {
-		const defaultFavicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
-		defaultFavicon.type = 'image/svg+xml';
-		defaultFavicon.rel = 'icon';
-		defaultFavicon.href = './src/assets/logo/logo-favicon.svg';
-		document.head.appendChild(defaultFavicon);
-	  }
+		if (configData && configData.title) {
+			document.title = configData.title;
+		} else {
+			document.title = 'Bật động sản';
+		}
+
+		if (configData && configData.icon) {
+			const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+			favicon.type = 'image/svg+xml';
+			favicon.rel = 'icon';
+			favicon.href = configData.icon;
+			document.head.appendChild(favicon);
+		} else {
+			const defaultFavicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+			defaultFavicon.type = 'image/svg+xml';
+			defaultFavicon.rel = 'icon';
+			defaultFavicon.href = './src/assets/logo/logo-favicon.svg';
+			document.head.appendChild(defaultFavicon);
+		}
 	} catch (error) {
-	  console.error('Error fetching config:', error);
-	  document.title = 'Thuê văn phòng'; 
+		console.error('Error fetching config:', error);
+		document.title = 'Thuê văn phòng';
 	}
-  }
-  
-  updateTitle();
+}
+
+updateTitle();
